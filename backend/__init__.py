e07c258de44e19a36b4800cb59bff00b55be6043
Caleb Dalgleish <Dalgleishc@uchicago.edu>
2023-04-07 18:00:36 -0500

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,1 +1,10 @@
-# code 
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+if __name__ == '__main__':
+    app.run()
